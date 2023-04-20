5890dfb78e64d08133913e2f4b8b5f09827196ad
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-04-20 13:04:04 +0300

    Adding Hero

---
+++ nextJs/tenniscenter/src/pages/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from '../../styles/Home.module.css'
+
+const Hero = () => {
+  return (
+    <section id={style.hero}>
+        <h1>TENNISCENTER</h1>
+    </section>
+  )
+}
+
+export default Hero
--- nextJs/tenniscenter/src/pages/index.tsx
+++ nextJs/tenniscenter/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className="flex w-full flex-col items-center">
       <Header />
+      <Hero />
     </main>
   )
 }
