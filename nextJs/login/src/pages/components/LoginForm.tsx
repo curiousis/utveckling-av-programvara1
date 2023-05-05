baf8e4f9d70f89915164f8e44e414c5786721a8d
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-05-05 10:38:06 +0300

    init

--- nextJs/login/src/pages/components/LoginForm.tsx
+++ nextJs/login/src/pages/components/LoginForm.tsx
@@ -1,21 +1,34 @@
 import React,{FormEvent, useState} from 'react'
 import style from '../../styles/Home.module.css'
+import { data } from 'autoprefixer';
 
 const LoginForm = () => {
 
-    const[username,setUserName] =useState('');
-    const [password,setPassword] = useState('')
-    const [visible,setVisible]= useState(false)
-    function handleSubmit(e:FormEvent){
-      e.preventDefault()
-      setVisible(true)
-    }
+  const[email,setEmail] =useState('');
+  const [password,setPassword] = useState('')
+  const [visible,setVisible]= useState(false)
+  async function handleSubmit(e:FormEvent){
+    e.preventDefault()
+    //setVisible(true)
+    const users = await fetchData();
+    await validateUser(users)
+  }
+
+  async function fetchData(){
+    const response = await fetch('http://10.111.3.78:3000/api/users')
+    const data:[any] = await response.json()
+    return data
+  }
+
+  async function validateUser(data:any[]){
+    data.includes(data.find(user => user.email === email)) ? window.location.href = '/profile' : null
+  }
 
   return (
     <div>
       <form className={` flex item-center flex-col w-75 justify-center gap-3 ${style.form}`} onSubmit={handleSubmit}>
-          <label htmlFor="username">Username</label>
-          <input type="text" id='username'className={style.input} onChange={(e)=>setUserName(e.target.value)}/>
+          <label htmlFor="email">Username</label>
+          <input type="text" id='email'className={style.input} onChange={(e)=>setEmail(e.target.value)}/>
           <label htmlFor="password">Password</label>
           <input type="password" id='password' className={style.input} onChange={(e)=>setPassword(e.target.value)}/>  
           <button type='submit' id={style.button}>Login</button>
@@ -23,7 +36,7 @@ const LoginForm = () => {
       { 
       visible ? 
         <div className='w-[50%] h-[50%] shadow-md rounded-md'>
-          <h2 className='text-xl'>You're signed in as :</h2><span className='text-xl text-amber-700'>{username}</span>
+          <h2 className='text-xl'>You're signed in as :</h2><span className='text-xl text-amber-700'>{email}</span>
           <div className="mb-3"></div>
           <h2 className='text-xl'>And your password is </h2><span className='text-xl text-amber-700'>{password}</span>
           <button onClick={()=>setVisible(false)}>Close</button>
