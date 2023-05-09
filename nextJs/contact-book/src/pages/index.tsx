5a4d0519fe80716055ce1254c74679edccadd4af
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-05-09 10:05:23 +0300

    Adding the contact book

--- nextJs/contact-book/src/pages/index.tsx
+++ nextJs/contact-book/src/pages/index.tsx
@@ -1,42 +1,58 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { useState } from 'react'
+
+import { FormEvent, useState } from 'react'
 import style from '../styles/Home.module.css'
-const inter = Inter({ subsets: ['latin'] })
+
+
 
 export default function Home() {
-    const [contact,setContact] = useState([{
-      name:'Isra Banda',
-      phonenumber:'0134324324',
-  },{
-    name:'Sara Ben',
-    phonenumber:'0423235421'},{
-    name:'John Sam',
-    phonenumber:'0423432343'
-  }])
+    const [contacts,setContact] = useState([
+      {
+        name:'Isra Banda',
+        phonenumber:'0134324324',
+      },
+      {
+        name:'Sara Ben',
+        phonenumber:'0423235421'},
+      {
+        name:'John Sam',
+        phonenumber:'0423432343'
+      }
+    ])
+    const [name,setName]=useState('')
+    const [phonenumber,setPhonenumber]= useState('')
 
+    function deleteObject(e:any){
+      e.target.parentElement.parentElement.remove();  
+    }
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-center`}
+      className={`flex min-h-screen flex-col items-center justify-center ${style.main}`}
     >
+      <div className={`flex justify-evenly items-center ${style.inputContainer}`}>
+        <input placeholder='name'onChange={(e)=>setName(e.target.value)}/>
+        <input placeholder='phone number' onChange={(e)=>setPhonenumber(e.target.value)}/>
+        <button onClick={()=>setContact([...contacts,{name:name,phonenumber:phonenumber}])}> Add </button>
+      </div>
       <h2 className={style.heading}>Contacts</h2>
       <table className={` text-center ${style.table}`}>
         <thead>
           <tr>
             <th>Name</th>
             <th>Phone Number</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
-          {contact.map(contact=>
+          {contacts.map(contact=>
             <tr>
               <td>{contact.name}</td>
               <td>{contact.phonenumber}</td>
+              <td><button onClick={deleteObject}>Delete</button></td>
             </tr>
           )}
         </tbody>
       </table>
-      <button className={style.button}>Add Contact</button>
     </main>
   )
 }
+
