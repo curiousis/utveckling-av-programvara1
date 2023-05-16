e477af9b4b03819ffec5de01d7fb5da9f4c8bba4
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-05-16 09:01:29 +0300

    update

--- nextJs/anim-app/src/pages/index.tsx
+++ nextJs/anim-app/src/pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6">
                 <path fill-rule="evenodd" d="M9.293 16.707a1 1 0 01-1.414-1.414L12.586 10H4a1 1 0 010-2h8.586l-4.293-4.293a1 1 0 011.414-1.414l6 6a1 1 0 010 1.414l-6 6z" clip-rule="evenodd" />
             </svg>
-
+ 
           </div>
           <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
