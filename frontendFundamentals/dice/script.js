c63f2aef8c36d5229c99a7029487b8e96b9dc185
Biringanine Amuri Israel <92646009+Israel-B-Amuri@users.noreply.github.com>
2023-05-22 12:49:23 +0300

    init

--- frontendFundamentals/dice/script.js
+++ frontendFundamentals/dice/script.js
@@ -5,7 +5,7 @@ const sides = ["one", "two", "three", "four", "five", "six"];
 button.addEventListener("click", () => {
   const randomNumber = Math.floor(Math.random() * sides.length);
   canvasDice.innerHTML = sides[randomNumber];
-  drawSideThree();
+  drawSideFive();
 });
 
 function drawSideOne() {
@@ -33,16 +33,151 @@ function drawSideTwo() {
 
 function drawSideThree() {
   const context = canvasDice.getContext("2d");
+  const dotRadius = 5;
+  const dotSpacing = 50;
+
   context.clearRect(0, 0, canvasDice.width, canvasDice.height);
+  const centerX = canvasDice.width / 2;
+  const centerY = canvasDice.height / 2;
+
   context.beginPath();
-  context.arc(canvasDice.width / 4.5, canvasDice.height / 5, 5, 0, 2 * Math.PI);
-  context.arc(canvasDice.width / 2, canvasDice.height / 2, 5, 0, 2 * Math.PI);
   context.arc(
-    (2 * canvasDice.width) / 3,
-    (3 * canvasDice.height) / 3.5,
-    5,
+    centerX - dotSpacing,
+    centerY - dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+
+  context.arc(centerX, centerY, dotRadius, 0, 2 * Math.PI);
+
+  context.arc(
+    centerX + dotSpacing,
+    centerY + dotSpacing,
+    dotRadius,
     0,
     2 * Math.PI
   );
   context.fill();
+  context.closePath();
+}
+
+function drawSideFour() {
+  const context = canvasDice.getContext("2d");
+  const dotRadius = 5;
+  const dotSpacing = 50;
+
+  context.clearRect(0, 0, canvasDice.width, canvasDice.height);
+
+  const centerX = canvasDice.width / 2;
+  const centerY = canvasDice.height / 2;
+  context.fillStyle = "black";
+
+  context.beginPath();
+  context.arc(
+    centerX - dotSpacing,
+    centerY - dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+  context.closePath();
+  context.fill();
+  context.beginPath();
+  context.arc(
+    centerX + dotSpacing,
+    centerY - dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+  context.closePath();
+  context.fill();
+  context.beginPath();
+  context.arc(
+    centerX - dotSpacing,
+    centerY + dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+  context.closePath();
+  context.fill();
+  context.beginPath();
+  context.arc(
+    centerX + dotSpacing,
+    centerY + dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+  context.closePath();
+  context.fill();
+}
+
+function drawSideFive() {
+  const context = canvasDice.getContext("2d");
+  const dotRadius = 5;
+  const dotSpacing = 50;
+
+  const centerX = canvasDice.width / 2;
+  const centerY = canvasDice.height / 2;
+
+  context.clearRect(0, 0, canvasDice.width, canvasDice.height);
+
+  context.beginPath();
+  context.arc(
+    centerX - dotSpacing,
+    centerY - dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+  context.closePath();
+  context.fill();
+  context.beginPath();
+  context.arc(
+    centerX + dotSpacing,
+    centerY + dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+  context.closePath();
+  context.fill();
+  context.beginPath();
+  context.arc(centerX, centerY, dotRadius, 0, 2 * Math.PI);
+  context.closePath();
+  context.fill();
+
+  context.beginPath();
+  context.arc(
+    centerX + dotSpacing,
+    centerY - dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+  context.closePath();
+  context.fill();
+
+  context.beginPath();
+  context.arc(
+    centerX - dotSpacing,
+    centerY + dotSpacing,
+    dotRadius,
+    0,
+    2 * Math.PI
+  );
+  context.closePath();
+  context.fill();
+}
+
+function drawSideSix() {
+  const context = canvasDice.getContext("2d");
+  const dotRadius = 5;
+  const dotSpacing = 50;
+  context.clearRect(0, 0, canvasDice.width, canvasDice.height);
+  const centerX = canvasDice.width / 2;
+  const centerY = canvasDice.height / 2;
 }
